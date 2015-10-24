0cf6b6622f0640204d545d99deb7b5332dc8805f
rrmarshall <rhnmrsh@aol.com>
2015-10-23 22:19:48 -0400

    Assignment 7, Arrays

--- src/dnddiceroller/DiceTower.java
+++ src/dnddiceroller/DiceTower.java
@@ -1,3 +1,4 @@
+
 package dnddiceroller;
 
 import java.util.ArrayList;
@@ -5,24 +6,79 @@ import java.util.List;
 
 /**
  * Dice Tower.
- * A Dice Tower is a tool used by serious gamers use to roll many dice at once.
- * It looks like this https://www.miniaturescenery.com/Images/PortableDiceTowerLarge.jpg
- * An instance of a dice tower is defined by the number panels it contains to help
- * provide a more regular distribution of die values. The die bounce from panel to
- * panel until they exit the dice tower at the bottom tray.
- * A dice tower will accept a collection of dice and reports their results when
- * they reach the tray at the bottom
- * @author Paul Scarrone
+ * A Dice Tower is a tool used by serious gamers to roll many dice at once.
+ *
+ * An instance of a dice tower is defined by 
+ *      the number of panels it contains to help provide a regular
+ *      distribution of die values.
+ * 
+ * The die bounce from panel to panel until they exit the dice tower
+ * at the bottom of they tray.
+ * 
+ * A dice tower will accept a collection of dice and report their results
+ * when they reach the tray at the bottom
+ */
+
+/**
+ *
+ * @author Rhonda Marshall
  */
 public class DiceTower {
-  final int PANEL_COUNT = 3;
-  List<Die> dice;
-
-  public DiceTower() {
-	this.dice = new ArrayList();
-  }
-  
-  public DiceTower(List dice) {
-	this.dice = dice;
-  }
+    private final int PANEL_COUNT = 3;
+    List<Die> dice;
+    int trayValue;
+    private final Boolean debug;   // This is for my purposes to debug this progam
+    
+    /**
+     * DiceTower constructors
+     * 
+     */
+    public DiceTower() {
+        this.dice = new ArrayList();
+        this.trayValue = 0;
+        this.debug = false;
+    }
+    
+    public DiceTower(List dice) {
+            this.dice = dice;
+            this.trayValue = 0;
+            this.debug = false;
+    }
+    
+    /**
+     * getTrayValue accessors method
+     * 
+     * @return trayValue 
+     */
+    public int getTrayValue() {
+        
+        return this.trayValue;
+        
+    }  // getTrayValue
+     
+    /**
+     * dropDice method will roll each die, the number of panels that are
+     * in the dice tower
+     * 
+     * accumulate the trayValue
+     */
+    
+    public void dropDice() {
+        // for each of the die in the array
+        for (Die theDice : this.dice) {
+            // roll the dice the number of panels in the tower, PANEL_COUNT
+            for (int j = 0; j < PANEL_COUNT; j++) { 
+                if (this.debug) System.out.printf("dropDice: Rolling %d\n", j);
+                theDice.roll();
+            }
+            if (this.debug) {
+                System.out.printf("dropDice: value is %d\n\n", theDice.getValue());
+            }
+            this.trayValue += theDice.getValue();
+        }
+        
+        if (this.debug)
+            System.out.printf("trayValue: value of both dice %d\n\n", this.trayValue);
+        
+    } // dropDice
 }
--- src/dnddiceroller/DnDDiceRoller.java
+++ src/dnddiceroller/DnDDiceRoller.java
@@ -1,3 +1,4 @@
+
 package dnddiceroller;
 
 import java.util.ArrayList;
@@ -5,50 +6,55 @@ import java.util.List;
 
 /**
  *
- * @author Paul Scarrone
+ * @author Rhonda Marshall
  */
 public class DnDDiceRoller {
 
-  /**
-   * Try out your dies and dice tower
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-	for(int i = 0; i< 1000; i++){
-	  int d6 = test_oneD6();
-	  if(d6 != -1){
-		System.out.println("Die Test Failed with Value: " + d6);
-	  }
-	  int tower = test_diceTowerWithTwoD6();
-	  if(tower != -1){
-		System.out.println("Tower Test Failed with Value: " + tower);
-	  }
-	}
-  }
-  
-  public static int test_oneD6(){
-	Die die = new Die(6);
-	die.roll();
-	int dieValue = die.value();
-	if(dieValue >= 1 && dieValue <= 6){
-	  return -1; // Means the die value is outside its bounds for a d6
-	}else{
-	  return dieValue;
-	}
-  }
-  
-  public static int test_diceTowerWithTwoD6(){
-	List<Die> dice = new ArrayList();
-	dice.add(new Die(6));
-	dice.add(new Die(6));
-	DiceTower tower = DiceTower(dice);
-	tower.dropDice();
-	int trayValue = tower.trayValue();
-	if(trayValue >= 2 && trayValue <= 12){
-	  return -1; // means the die value is outside the bounds of 2 d6
-	}else{
-	  return trayValue;
-	}
-  }
-  
+    /**
+     * Try out your dies and dice tower
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Start Test");
+        for (int i = 0; i < 1000; i++) {
+            int d6 = test_oneD6();
+            if (d6 != -1) {
+                System.out.println("Die Test Failed with Value: " + d6);
+            }
+            int tower = test_diceTowerWithTwoD6();
+            if (tower != -1) {
+                System.out.println("Tower Test Failed with Value: " + tower);
+            }
+        }
+        System.out.println("End Test");
+    } // main method
+    
+    public static int test_oneD6() {
+        Die die = new Die(6);
+        die.roll();
+        
+        int dieValue = die.getValue();
+        if (dieValue >= 1 && dieValue <= 6) {
+            return -1; // Means the die value is outsides its bounds for a die
+        } else {
+            return dieValue;
+        }
+    } // test_oneD6 method
+    
+    public static int test_diceTowerWithTwoD6() {
+            List<Die> dice = new ArrayList();
+            dice.add(new Die(6));
+            dice.add(new Die(6));
+            
+            DiceTower tower = new DiceTower(dice);
+            tower.dropDice();
+            
+            int trayValue = tower.getTrayValue();
+            if (trayValue >= 2 && trayValue <= 12) {
+                return -1;
+            } else {
+                return trayValue;
+            }
+    } // test_diceTowerWithTwoD6 method
+    
 }
